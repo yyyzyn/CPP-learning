e47007e3d8d6473ecd3da8f7c3fe27fb76534ff9
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2023-02-03 16:38:59 +0800

    Add files via upload

---
+++ CPP函数学习/test3.4.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+#include "test3.4.h"
+
+
+int main()
+{
+    int a=10;
+    int b=20;
+
+    swap(a,b);
+    return 0;
+}
---
+++ CPP函数学习/test3.4.cpp
@@ -0,0 +1,15 @@
+#include "test3.4.h"
+using namespace std;
+//函数分文件编写//
+//实现两个数字进行交换的函数//
+
+void swap(int a,int b)
+{
+    int temp=a;
+    a=b;
+    b=temp;
+
+    cout<<"a= "<<a<<endl;
+    cout<<"b= "<<b<<endl;
+}
+
