dcb47ff3d4b47091d8b3977e1e134e18bef0aa25
仲启翔 <87054720+yyyzyn@users.noreply.github.com>
2023-02-03 16:31:03 +0800

    Add files via upload

---
+++ CPP基础语法/test2_5.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int i,sum=0;
+  while(i<=10)//while条件//
+  {
+      sum+=i;//具体执行内容//
+      i++;
+  }
+  cout<<"sum= "<<sum<<endl;
+  return 0;
+}
